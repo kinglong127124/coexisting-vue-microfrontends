236ede9e3f58e35999b7d0fe5be02f3282b86e46
zhengjl <12345678>
2019-10-11 09:40:13 +0800

    新增admin-front

---
+++ admin-front/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="admin-front">
+    admin-front is working
+    <p>comment me on and off to see HMR</p>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style>
+#admin-front {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
